2cb4ec4a7c498ada16ae548b6c31b815dba18add
JemIsseo <libertyjeremy23@gmail.com>
2022-11-02 14:28:37 +0800

    first commit

---
+++ Source Code/HTMLandPHP/index.php
@@ -0,0 +1,55 @@
+<?php 
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Account</title>
+    <link rel="stylesheet" href="../CSS/style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Questrial&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" 
+    integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" 
+    crossorigin="anonymous">
+</head>
+
+<body>
+        <!-- Login Form Section -->
+    <main>
+        <section class = "glassloginform">
+            <h2 class="logintext">User Login</h2>
+            <form action="dashboard.php" method="POST">
+                <div class="input-box">
+                    <i class="fa-sharp fa-solid fa-user"></i>
+                    <input type="text" name="username" placeholder="Username">
+                </div>
+                <div class="input-box">
+                    <i class="fa-sharp fa-solid fa-lock"></i>
+                    <input type="password" name="password" placeholder="Password">
+                </div>
+                    <button name="login" type="submit" class="login-btn">Login</button>
+            </form>
+        </section>
+    </main>
+    <div class="circle1"></div>
+    <div class="circle2"></div>
+    <div class="circle3"></div>
+    <div class="circle4"></div>
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
